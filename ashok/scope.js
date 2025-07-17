938731ca9cda4f5301b45b3411f3434088b35e0d
AshokCloudRank <ashok@cloud-rank.com>
2025-07-17 18:43:35 +0530

    file push

---
+++ ashok/scope.js
@@ -0,0 +1,35 @@
+function Dasna(){
+    const id = "Cloud Rank"
+    function Ghaziabad(){
+        const id1 = "Noida"
+       // console.log(id);
+        
+    }
+    //console.log(id1);
+    Ghaziabad()
+    
+}
+Dasna()
+
+
+if (true) {
+    const userId = "Kohali"
+    if (userId === "Kohali"){
+        const userName = " Ashok"
+        //console.log(userId + userName);
+    }
+    //console.log(userName);
+}
+//console.log(userId);
+
+// *******************************
+
+console.log(x(7))
+function x(num){
+    return num + 1
+}
+
+y(9)
+const y = function(num){
+    return num + 3
+}
